3d3aeac46dee873f3081972ab680093a999a337e
Tara Nelson & robin miller <student@epicodus.com>
2014-04-10 09:42:31 -0700

    save resized versions of uploaded photos and display the small version by default

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ActiveRecord::Base
   has_many :tags
   belongs_to :user
 
-  has_attached_file :photo
-  validates_attachment :photo, presence: true, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
+  has_attached_file :photo, :styles => { :small => "150x150>", :medium => "300x300>", :large => "400x400>"},
+      :url => "/assets/posts/:id/:style/:basename.:extension",
+      :path => ":rails_root/public/assets/posts/:id/:style/:basename.:extension"
+
+  validates_attachment :photo, presence: true, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }, size: {less_than: 5.megabytes}
+
 end
